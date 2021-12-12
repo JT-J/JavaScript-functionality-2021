42637347b60bfc2f66a8608ee54254e1d97fd3ce
Jonathan Johansson <52955548+JT-J@users.noreply.github.com>
2021-12-12 22:25:37 +0100

    Uploaded the guestbook.js and json package.

---
+++ Guestbook.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+//const uuid = require("uuid");
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.get("/commentSubmited", function(request, response){
+    fs.readFile( "./myfile.json", "utf8", (error, result)=>{
+     if (error) {
+         console.log(error);
+         return;
+     }
+     const clientJson = JSON.parse(result);
+     response.json(clientJson).end();
+     // return json(clientJson);
+    }) 
+ });
+
+ function replacer(key, value){
+    if (typeof value === "string"){
+
+        return value.replaceAll("<","&lt;").replaceAll(">", "&gt;")
+    }
+    return value;
+}
+
+app.post('/commentSubmited', function (request, response) {
+    fs.readFile( "./myfile.json", "utf8", (error, result)=>{
+        if (error) {
+            console.log(error);
+            return;
+        }
+        let newComment = request.body;
+        newComment.id = uuid.v4(); // Genererar ett slumpat ID.
+        newComment.likes = 0;
+
+        const clientJson = JSON.parse(result);
+        clientJson.push(newComment) // array.
+
+        fs.writeFile("./myfile.json", JSON.stringify(clientJson, replacer, 2),(error, result) =>{ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#the_replacer_parameter
+            if (error) {
+                console.log(error);
+                return;
+            }
+            response.json({}).end();
+        })
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running at localhost: ${port}`);
+    });
